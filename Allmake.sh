b2237364a4fee3998eda16183cd8b6240176a3a8
petik13 <nikos.petik@gmail.com>
2025-12-14 17:35:24 +0100

    Include Allmake.sh

---
+++ Allmake.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd applications/solvers/PHIWaveCurSph2
+wclean
+wmake
+cd -
+cd src/functionObjects/forces/meanWaveLoads/
+wclean
+wmake
+cd -
+cd src/finiteVolume/fields/fvPatchFields/derived/waveCurMqLeastAB3DPotUPFD5/
+wclean
+wmake
+cd -
